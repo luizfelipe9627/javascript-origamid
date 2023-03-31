3500e695a0610f691bfe8bcd61c5d9fd7dc9c912
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-03-31 16:42:16 -0300

    update

--- boleean-e-condicionais/script-exercise.js
+++ boleean-e-condicionais/script-exercise.js
@@ -1,30 +1,54 @@
 // Verifique se a sua idade é maior do que a de algum parente.
 // Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+let minhaIdade = 19;
+let maeIdade = 38;
+let paiIdade = 42;
+
+if (minhaIdade > maeIdade) {
+  console.log("É maior.");
+} else if (minhaIdade === maeIdade) {
+  console.log("É igual.");
+} else {
+  console.log("É menor.");
+}
 
 // Qual valor é retornado na seguinte expressão?
-var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+var expressao = 5 - 2 && 5 - " " && 5 - 2;
+console.log(expressao); // 3
 
 // Verifique se as seguintes variáveis são Truthy ou Falsy
-var nome = 'Andre';
-var idade = 28;
-var possuiDoutorado = false;
-var empregoFuturo;
-var dinheiroNaConta = 0;
+var nome = "Andre"; // true
+var idade = 28; // true
+var possuiDoutorado = false; // false
+var empregoFuturo; // false
+var dinheiroNaConta = 0; // false
+console.log(
+  !!nome,
+  !!idade,
+  !!possuiDoutorado,
+  !!empregoFuturo,
+  !!dinheiroNaConta
+);
 
 // Compare o total de habitantes do Brasil com China (valor em milhões)
 var brasil = 207;
 var china = 1340;
-
-// O que irá aparecer no console?
-if(('Gato' === 'gato') && (5 > 2)) {
-  console.log('Verdadeiro');
+if (brasil > china) {
+  console.log("Brasil tem mais habitantes que a China.");
 } else {
-  console.log('Falso');
+  console.log("Brasil tem menos habitantes que a China.");
 }
 
 // O que irá aparecer no console?
-if(('Gato' === 'gato') || (5 > 2)) {
-  console.log('Gato' && 'Cão');
+if ("Gato" === "gato" && 5 > 2) {
+  console.log("Verdadeiro");
 } else {
-  console.log('Falso');
+  console.log("Falso"); // Irá retornar falso
+}
+
+// O que irá aparecer no console?
+if ("Gato" === "gato" || 5 > 2) {
+  console.log("Gato" && "Cão"); // Cão
+} else {
+  console.log("Falso");
 }
--- boleean-e-condicionais/script.js
+++ boleean-e-condicionais/script.js
@@ -3,10 +3,10 @@ let possuiGraduacao = true;
 let possuiMBA = false;
 
 // Se o valor for true(verdadeiro) irá executar o primeiro bloco de comando.
-if(possuiGraduacao) {
+if (possuiGraduacao) {
   console.log("Possui graduação.");
 } // O else if verifica se a condição anterior é false, se for ele verifica o else if, se não for true ele irá para o else.
- else if(possuiMBA) {
+else if (possuiMBA) {
   console.log("Possui MBA.");
 } // Se for false ele ira executar o segundo bloco no c else.
 else {
@@ -14,12 +14,90 @@ else {
 }
 
 // Caso a variável esteja vazia ela é false, se tiver algo ela é true.
-let nome = '';
+let nome = "";
 // Se nome existir então ele irá executar o comando dentro do if.
-if(nome) {
-  console.log(nome)
+if (nome) {
+  console.log(nome);
   // Caso contrario irá executar o comando dentro do else.
 } else {
-  console.log('Não existe nome.');
+  console.log("Não existe nome.");
 }
 
+// Operador de comparação maior(">") verifica se o primeiro valor é maior que o segundo, se for retorna true, caso contrario false.
+let maior = 2 > 5;
+console.log(maior); // É false pois 2 não é maior que 5.
+// Operador de comparação menor("<") verifica se o primeiro valor é menor que o segundo, se for retorna true, caso contrario false.
+let menor = 5 < 10;
+console.log(menor); // É true pois 5 é menor que 10.
+// Operador de comparação menor igual("<=") verifica se o primeiro valor é menor ou igual ao segundo, se for retorna true, caso contrario false.
+let menorIgual = 10 <= 10;
+console.log(menorIgual); // É true pois 10 é igual a 10.
+// Operador de comparação menor igual("<=") verifica se o primeiro valor é menor ou igual ao segundo, se for retorna true, caso contrario false.
+let maiorIgual = 10 >= 11;
+console.log(maiorIgual); // É false pois 10 não é maior ou igual a 11.
+
+// O operador lógico de negação("!") inverte os valores, sendo assim o true vira false e vice versa. Assim executando o comando dentro do if.
+if (!possuiMBA) {
+  console.log("Não possui MBA.");
+}
+// Para verificar se algo é true ou false podemos usar duas negações ("!!"), assim fazendo ele negar e depois voltar ao valor original.
+if (!!possuiMBA) {
+  console.log("Não possui MBA.");
+} else {
+  console.log("Possui MBA.");
+}
+
+// O operador lógico de comparação igual("==") diz por true ou false se o valor é igual um do outro, sem restringir o tipo.
+let igualNum = "10";
+// Irá retornar no console true pois eles são o mesmo número.
+console.log(igualNum == 10);
+// O operador lógico de comparação igual("===") diz por true ou false se o valor é igual um do outro, restringindo o tipo.
+let igualTipo = "23";
+// Irá retornar no console false pois eles não são do mesmo tipo.
+console.log(igualTipo === 23);
+
+// O operador lógico diferente("!=") diz por true ou false se o valor é diferente um do outro, sem restringir o tipo.
+let diferenteNumber = "451";
+// Irá retornar no console true pois eles são diferentes.
+console.log(diferenteNumber != 23);
+// O operador lógico diferente("!==") diz por true ou false se o valor é diferente um do outro, restringindo o tipo.
+let diferenteString = "Cachorro";
+// Irá retornar no console false pois eles não são diferentes.
+console.log(diferenteString !== "Cachorro");
+
+// O operador lógico and("&&") só retorna true caso os dois valores sejam verdadeiros, se algum ser false ele irá ignorar o true e vai retornar o false.
+let operadorAnd = 5 - 10 && 5 + 5;
+if (operadorAnd) {
+  console.log(
+    "Verdadeiro, pois as duas condições são verdadeiras.",
+    operadorAnd
+  );
+} else {
+  console.log("Falso, pois nenhuma condição é verdadeira.", operadorAnd);
+}
+
+// O operador lógico ou("||") só retorna true caso algum dos valores for true, se os dois for false ele irá retornar false.
+var operadorOr = 5 - 5 || 5 + 5 || 10 - 2;
+if (operadorOr) {
+  console.log("Verdadeiro, pois um dos valores é verdadeiro.", operadorOr);
+} else {
+  console.log("Falso, pois nenhum número é verdadeiro.", operadorOr);
+}
+
+let corFavorita = "Azul";
+// O switch case é usado para verificar se um valor é igual a uma das opções definidas, se for ele irá executar.
+// O break do case finaliza a execução do switch quando encontrado a resposta.
+// O default é acionado quando nenhuma das opções for verdadeira.
+switch (corFavorita) {
+  case "Azul":
+    console.log("Olhe para o céu.");
+    break;
+  case "Vermelho":
+    console.log("Olhe para rosas.");
+    break;
+  case "Amarelo":
+    console.log("Olhe para o sol.");
+    break;
+  default:
+    console.log("Feche os olhos.");
+}
