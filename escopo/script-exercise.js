3740dcda928fba9256f1c093fd0471e08f33d9c8
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-04-03 21:54:47 -0300

    update

--- escopo/script-exercise.js
+++ escopo/script-exercise.js
@@ -1,28 +1,34 @@
 // Por qual motivo o código abaixo retorna com erros?
 {
-  var cor = 'preto';
-  const marca = 'Fiat';
+  var cor = "preto";
+  const marca = "Fiat";
   let portas = 4;
+
+  console.log(cor, marca, portas); // * Tem que ficar no mesmo bloco ou escopo.
 }
-console.log(var, marca, portas);
 
 // Como corrigir o erro abaixo?
+const dois = 2; // * Como é usado em diferentes funções tem que ser uma variável global.
+
 function somarDois(x) {
-  const dois = 2;
   return x + dois;
 }
+
 function dividirDois(x) {
-  return x + dois;
+  return x / dois;
 }
-somarDois(4);
-dividirDois(6);
+
+console.log(somarDois(4));
+console.log(dividirDois(6));
 
 // O que fazer para total retornar 500?
-var numero = 50;
+const numero = 50; // * Foi mudado de var para const para ele não for pego no for.
 
-for(var numero = 0; numero < 10; numero++) {
+// * Mudado de var para let, que é o apropriado para usar em loops.
+for (let numero = 0; numero < 10; numero++) {
   console.log(numero);
 }
 
 const total = 10 * numero;
+
 console.log(total);
---
+++ escopo/script.js
@@ -0,0 +1,58 @@
+"use strict"; // Serve para não permitir que erros como uma variável sem as palavras, let, var e const não aconteçam.
+
+// * Escopo local e global.
+
+function showCar() {
+  let carFusca = "Fusca"; // Criado uma variável local, no caso só vai ser reconhecida dentro da função.
+  console.log(carFusca); // Irá retornar "Fusca" no console.
+}
+
+showCar(); // Está invocando a função.
+
+// console.log(carFusca); // Irá retornar "erro, car is not defined", a variável car está definida somente no escopo da função.
+
+let carFerrari = "Ferrari"; // Criado uma variável global, no caso vai ser reconhecida em qualquer tipo de escopo.
+
+console.log(carFerrari); // Agora irá mostrar no console pois está no escopo global.
+
+// * O porque não utilizar o VAR.
+
+if (true) {
+  var month = "December"; // Utilizar o var não é recomendado pois ele ignora o contexto de escopo. É bom utilizar sempre let ou const que respeitam o escopo.
+  console.log(month); // Irá retornar "December" no console.
+}
+
+console.log(month); // Irá funcionar também pois o var está ignorando o escopo que ele está.
+
+// * Variável const.
+
+const week = "Friday";
+console.log(week);
+
+// week = 'Saturday'; // Irá retornar erro pois o const não permite ter seu valor alterado.
+
+// const week = 'Monday'; // Irá retornar erro pois o const não permite criar a mesma constante mas com valor diferente.
+
+// const empty; // Irá retornar erro pois também não permite deixar uma constante vazia.
+
+const date = {
+  year: 2024,
+  month: "February",
+};
+
+date.year = 2022; // Funciona pois está alterando a propriedade do objeto e não o objeto em si.
+
+date.days = 31; // Também funciona pois está adicionando uma nova propriedade no objeto.
+// date = 'Uma string'; // Retorna erro pois está tentando alterar o objeto.
+
+console.log(date);
+
+// * Variável let.
+
+let myYear; // Permite criar uma variável vazia.
+
+myYear = 2003; // Permite soobrescrever o valor da variável
+
+console.log(myYear); // 2003
+
+// let myYear = 2000; // Retorna erro, pois o let não aceita redeclarar a mesma variável.
