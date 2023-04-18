b3176edf55fc030f0d04587f24425d47afbfb3ad
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-04-18 16:45:00 -0300

    update

---
+++ objetos/constructor-functions/script.js
@@ -0,0 +1,7 @@
+// Criado uma constante contendo um objeto chamado car.
+const car = {
+  // Criou várias propriedades de tipos diferentes no objeto.
+  brand: 'Genérica',
+  price: 0,
+}
+console.log(car);
