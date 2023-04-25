37f9056661d158fd0871ffb1472012d040958e81
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-04-24 22:45:31 -0300

    update

---
+++ objetos/number-e-math/script.js
@@ -0,0 +1,11 @@
+//* Definindo e criando number.
+
+const year = 2023; // Criado uma variável chamada year que armazena o number 2023.
+console.log(year); // Retorna o number no console.
+const price = new Number(100); // Cria um objeto do tipo construtor number que recebe o valor 100.
+console.log(price); // Retorna o objeto construtor no console.
+
+//* Métodos e propriedades de um number.
+
+
+
