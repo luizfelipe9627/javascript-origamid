4cdf53e18136151baf86139395849c25d9f6e266
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-04-27 16:48:54 -0300

    update

--- objetos/array/script-exercise.js
+++ objetos/array/script-exercise.js
@@ -4,18 +4,45 @@
 // Adicione 'Peixe' e 'Batata' ao início da array.
 const comidas = ["Pizza", "Frango", "Carne", "Macarrão"];
 
+const primeiraComida = comidas.shift();
+console.log(primeiraComida);
+
+const ultimaComida = comidas.pop();
+console.log(ultimaComida);
+
+comidas.push("Arroz");
+comidas.unshift("Peixe", "Batata");
+console.log(comidas);
+
 // Arrume os estudantes em ordem alfabética.
 // Inverta a ordem dos estudantes.
 // Verifique se Joana faz parte dos estudantes.
 // Verifique se Juliana faz parte dos estudantes.
 const estudantes = ["Marcio", "Brenda", "Joana", "Kleber", "Julia"];
 
+estudantes.sort();
+estudantes.reverse();
+
+console.log(estudantes.includes("Joana"));
+console.log(estudantes.includes("Juliana"));
+
 // Substitua section por ul e div com li, utilizando split e join.
-let html = `<section>
-              <div>Sobre</div>
-              <div>Produtos</div>
-              <div>Contato</div>
-            </section>`;
+let html = `
+<section>
+  <div>Sobre</div>
+  <div>Produtos</div>
+  <div>Contato</div>
+</section>`;
+
+html = html.split("section").join("ul");
+html = html.split("div").join("li");
+console.log(html);
 
 // Remova o último carro, mas antes de remover salve a array original em outra variável.
 const carros = ["Ford", "Fiat", "VW", "Honda"];
+
+const carrosCopia = carros.slice();
+console.log(carrosCopia);
+
+carros.pop();
+console.log(carros);
--- objetos/array/script.js
+++ objetos/array/script.js
@@ -2,6 +2,7 @@
 
 const citys = ["São Paulo", "Rio de Janeiro", "Ribeirão Preto"]; // Criado uma array contendo 3 elementos do tipo string.
 console.log(citys); // Retorna a array no console.
+
 const price = [49, 99, 69, 89]; // Criado uma array contendo 4 elementos do tipo number.
 console.log(price); // Retorna a array no console.
 
@@ -21,6 +22,7 @@ console.log(datas[1][2].cor); // Está acessando o segundo elemento, no caso o o
 
 const instruments = ["Guitarra", "Baixo", "Violão"]; // Criado uma array comum contendo 3 elementos.
 console.log(instruments); // Retorna a array no console.
+
 const car = new Array("Corola", "Mustang", "Honda"); // Criado uma array chamada car que está recebendo 3 elementos.
 console.log(car); // Retorna a nova array no console.
 
@@ -69,11 +71,13 @@ console.log(frutas[2][0].length); // Retorna 8 no console pois ele ta contando q
 //* > Método modificador sort.
 
 const names = ["Zac", "Laura", "Carlos", "Felipe"]; // Criado uma array contendo 4 elementos string.
+
 // O método sort é usado para ordenar os itens de uma array em ordem alfabética.
 console.log(names.sort()); // Retorna a array com as string organizados por ordem alfabética.
 console.log(names); // Modificou a array, sendo assim reescreveu a array original.
 
 const ages = [32, 21, 33, 43, 1, 12, 8]; // Criado uma array contendo 7 elementos number.
+
 // O método sort também pode ser usado para ordenar números a partir do seu caractere, sendo assim ele irá primeira ordenar do menor para o maior contando o primeiro caractere e depois de terminar todos ele vai para o segundo.
 console.log(ages.sort()); // Retorna a array com os números em ordem crescente.
 console.log(ages); // Modificou a array, sendo assim reescreveu a array original.
@@ -81,6 +85,7 @@ console.log(ages); // Modificou a array, sendo assim reescreveu a array original
 //* > Método modificador unshift.
 
 const carBrand = ["Ford", "Fiat", "VW"]; // Criado uma array chamada carBrand que contém 3 elementos do tipo string.
+
 // O método unshift é usado para adicionar elementos ao inicio da array e retorna o length(tamanho) da array.
 console.log(carBrand.unshift("Honda", "Kia")); // Adiciona os elementos no inicio da array e retorna o tamanho da array no console, no caso 5.
 console.log(carBrand); // Retorna a array com os dois elementos criados no inicio dela.
@@ -112,12 +117,71 @@ console.log(carBrand); // Retorna a array modificada, sendo assim ele invertido.
 //* > Método modificador splice.
 
 // O método splice é usado para adicionar valores na array a partir do seu index, também remove a quantidade de itens que for passada no segundo parâmetro e retorna eles.
-console.log(carBrand.splice(1, 0, 'Uno', 'Chevrolet')); // Adiciona os elementos a partir do index 1 e remove 0 elementos, retorna no console uma array vazia.
+console.log(carBrand.splice(1, 0, "Uno", "Chevrolet")); // Adiciona os elementos a partir do index 1 e remove 0 elementos, retorna no console uma array vazia.
 console.log(carBrand); // Retorna no console os elementos adicionados a partir do index 1.
 
-console.log(carBrand.splice(3, 2, 'Uno', 'Chevrolet')); // Adiciona os elementos a partir do index 3 e remove 2 elementos da array, retorna os elementos da array no console.
+console.log(carBrand.splice(3, 2, "Uno", "Chevrolet")); // Adiciona os elementos a partir do index 3 e remove 2 elementos da array, retorna os elementos da array no console.
 console.log(carBrand); // Retorna no console os elementos adicionados a partir do index 3 mas sem os elementos removidos.
 
 //* > Método modificador copyWithin.
 
+// O método copyWithin é usado para a partir do alvo, ele irá copiar a array começando do inicio até o final e vai preencher a mesma com essa cópia. Caso omita os valores de início e final, ele irá utilizar como inicio o 0 e final o valor total da array.
+console.log(["Item1", "Item2", "Item3", "Item4", "Item5"].copyWithin(2, 0, 2)); // Retorna ['Item1', 'Item2', 'Item1', 'Item2', 'Item3'] no console pois vai começar a modificar a partir do terceiro elemento, e faz uma copia da array a partir do primeiro item até o terceiro item.
+console.log(["Item1", "Item2", "Item3", "Item4", "Item5"].copyWithin(-1)); // Retorna ['Item1', 'Item2', 'Item3', 'Item1'] no console, pois ele começa pelo final.
 
+//* > Método modificador fill.
+
+// O método fill preenche a array com o valor passado, sendo assim, os valores de inicio e fim.
+console.log(["Item1", "Item2", "Item3", "Item4"].fill("Banana")); // Retorna ['Banana', 'Banana', 'Banana', 'Banana'] no console, pois não foi passado nenhum valor de inicio ou fim.
+console.log(["Item1", "Item2", "Item3", "Item4"].fill("Banana", 2)); // Retorna ['Item1', 'Item2', 'Banana', 'Banana'] no console pois começa a modificar a partir do terceiro index.
+console.log(["Item1", "Item2", "Item3", "Item4"].fill("Banana", 1, 3)); // Retorna ['Item1', 'Banana', 'Banana', 'Banana'] no console, pois começa a modificar a partir do index 1 e termina no index 3.
+
+//* > Método de acesso concat.
+
+const transport1 = ["Barco", "Aviao"]; // Criado uma array chamada transport1 contendo dois elementos do tipo string.
+const transport2 = ["Carro", "Moto"]; // Criado uma array chamada transport2 contendo dois elementos do tipo string.
+
+// O método concat é usado para concatenar/juntar os elementos de uma array com outra.
+// Obs.: Os métodos de acesso não modificam a array, sendo assim os seus valores iniciais não são alterados.
+const transports = transport1.concat(transport2); // Está concatenando os elementos do transport1 com o do transport2.
+console.log(transports); // Irá retornar no console os elementos das arrays concatenadas.
+
+const moreTransports = [].concat(transport1, transport2, "Van"); // Passado uma array vazia e que está concatenando os elementos do transport1 com o do transport2. e adicionando um novo elemento na array.
+console.log(moreTransports); // Retorna no console os elementos da array concatenados.
+
+//* > Método de acesso includes, indexOf E indexLastOf.
+
+const languages = ["HTML", "CSS", "JS", "PHP", "PYTHON", "JS"];
+
+// O includes é um método responsável por retornar se um valor/elemento existe na array através de valor boolean.
+console.log(languages.includes("CSS")); // Existe na array então irá retornar true.
+console.log(languages.includes("RUBY")); // Não existe na array então irá retornar false.
+
+// O indexOf é método usado para verificar se a array possui o valor e retorna o index do primeiro valor encontrado na array.
+console.log(languages.indexOf("PYTHON")); // Está no index 4, então retorna 4 no console.
+console.log(languages.indexOf("JS")); // Está no index 2(primeiro encontrado), então retorna 2 no console.
+
+// O lastIndexOf retorna o último index encontrado e não o primeiro como o indexOf.
+console.log(languages.lastIndexOf("JS")); // Está no index 5(ultimo encontrado), então retorna 5 no console
+
+//* > Método de acesso join.
+
+// O método join é usado para juntar todos os valores da array e retorna uma string com eles. Se você passar um valor como parâmetro, este será utilizado durante a junção de cada item da array.
+console.log(languages.join()); // Retorna a array em uma string, separando por virgulas(padrão caso o join esteja sem nada).
+console.log(languages.join(" ")); // Retorna a array tendo cada item separada por espaço.
+console.log(languages.join("-_-")); // Retorna a array tendo cada item seu separado por caracteres definidos.
+
+let htmlString = '<h2>Título Principal</h2>'; // Criado uma string contendo um elemento HTML.
+htmlString = htmlString.split('h2'); // O split é um método usada para separar/dividir um elemento a partir de tal parte(nesse caso a cada h2) e separando/transformando ela em uma array.
+console.log(htmlString); // Retorna no console uma array separada pelos H2..
+htmlString = htmlString.join('h1'); // O join está transformando em string novamente e preenchendo os espaços em brancos pelo elemento H1.
+console.log(htmlString); // Retorna no console uma string contendo H1.
+
+//* > Método de acesso slice.
+
+// O método slice recebe dois parâmetros, o inicio e o valor final e por fim retorna os itens da array selecionados.
+console.log(languages.slice(3)); // Retorna a partir do index 3.
+console.log(languages.slice(1, 4)); // Retorna a array começando do index 1 até o index 4
+
+const cloneLanguages = languages.slice(); // Podemos evitar de passar valores dentro para usar o slice para clonar uma array.
+console.log(cloneLanguages); // Mostra a array clonada.
