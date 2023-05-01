36469cdb1a1f29f6065391615d60a21ba785f60e
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-05-01 20:07:58 -0300

    update

--- objetos/array-e-iteracao/script-exercise.js
+++ objetos/array-e-iteracao/script-exercise.js
@@ -1,10 +1,35 @@
 // Selecione cada curso e retorne uma array com objetos contendo o título, descricao, aulas e horas de cada curso.
+const cursos = document.querySelectorAll(".curso");
+const cursosArray = Array.from(cursos);
+
+const objetosCursos = cursosArray.map((curso) => {
+  const titulo = curso.querySelector("h1").innerText;
+  const descricao = curso.querySelector("p").innerText;
+  const aulas = +curso.querySelector(".aulas").innerText;
+  const horas = +curso.querySelector(".horas").innerText;
+
+  return {
+    // Quando se tem o mesmo nome, pode ser passado apenas o nome da variável que por padrão ira definir esse nome na propriedade do objeto.
+    titulo,
+    descricao,
+    aulas,
+    horas,
+  };
+});
+
+console.log(objetosCursos);
 
 // Retorne uma lista com os números maiores que 100.
 const numeros = [3, 44, 333, 23, 122, 322, 33];
 
+const maioresQue100 = numeros.filter((num) => num > 100);
+console.log(maioresQue100);
+
 // Verifique se Baixo faz parte da lista de instrumentos e retorne true.
 const instrumentos = ["Guitarra", "Baixo", "Bateria", "Teclado"];
+
+const possuiBaixo = instrumentos.some((item) => item === "Baixo");
+console.log(possuiBaixo);
 
 // Retorne o valor total das compras.
 const compras = [
@@ -29,3 +54,20 @@ const compras = [
     preco: "R$ 10,60",
   },
 ];
+
+const valorTotal = compras.reduce((acumulador, item) => {
+  const precoLimpo = +item.preco.replace("R$ ", "").replace(",", ".");
+  return acumulador + precoLimpo;
+}, 0);
+
+console.log(valorTotal);
+
+// Mesmo resultado mas com solução diferente:
+let precoTotal = 0;
+
+const precoCompras = compras.map((item) => {
+  item = +item.preco.replace("R$ ", "").replace(",", ".");
+  return (precoTotal += item);
+});
+
+console.log(precoTotal);
--- objetos/array-e-iteracao/script.js
+++ objetos/array-e-iteracao/script.js
@@ -105,17 +105,139 @@ const reduceClasses = classesMinutes.reduce(
     console.log(acumulador, item, index); // Mostra todos os valores no console.
     return item + acumulador; // Retorna o item para não ficar undefined no console mais o valor do acumulador.
   },
-  0 // Está definindo o valor inicial como 0(importante definir um valor se não pula a primeira iteração).
-); 
+  0 // Está definindo o valor inicial(o parâmetro acumulador) como 0(importante definir um valor se não pula a primeira iteração).
+);
 console.log(reduceClasses); // Retorna a soma de cada acumulador e item no console.
 
 //* > Usando o reduce para retornar o maior número de uma array.
 
 const numbersList = [10, 25, 60, 5, 35, 10]; // Criado uma array chamada numbersList que contém 6 itens do tipo number.
 
-// O reduce passa por cada item
-const greaterValue = numbersList.reduce((anterior, atual) => {
-  return anterior < atual ? atual : anterior;
-});
+// O reduce passa por cada item tendo dois parâmetros(o valor anterior, e o valor atual que é o que realmente passa por cada elemento da array).
+const higherNumber = numbersList.reduce((anterior, atual) => {
+  // Se anterior for maior que atual irá executar o if.
+  if (anterior > atual) {
+    return anterior; // Retorna o número anterior, fazendo assim ele preencher o parâmetro atual do método.
+  }
+  // Se não, irá executar o else.
+  else {
+    return atual; // Retorna o número atual, fazendo ele ocupar o lugar do parâmetro atual do método.
+  }
+}, 0); // Está definindo o valor inicial(o parâmetro anterior) como 0(importante definir um valor para não ter risco de pular a primeira iteração).
 
-console.log(greaterValue); // Retorna 60 no console pois ele foi o maior número da array.
+console.log(higherNumber); // Retorna 60 no console pois ele foi o maior número da array.
+
+//* > Usando o método reduce para objetos.
+
+// O reduce passa por cada item tendo dois parâmetros(o valor anterior(nomeado como acumulador), o valor atual que é o que realmente passa por cada elemento da array e também o index que é a posição de cada elemento na array.
+const listClasses = classes.reduce((acumulador, atual, index) => {
+  acumulador[index] = atual.name; // Está pegando o acumulador(um objeto vazio), e passando como índice da array vazia o index da array classes, e atribuindo no elemento atual(o que percorre cada objeto) a propriedade name.
+  console.log(acumulador); // Mostra o acumulador a cada loop no console.
+  return acumulador; // Retorna o acumulador e atribui ao parâmetro esse valor vai ser mudado a cada loop.
+}, {});
+
+//* > Método reduceRight.
+
+const fruits = ["Banana", "Pêra", "Uva"];
+
+// O método reduce e o reduceRight são praticamente iguais, a diferença é que este começa a iterar da direita para a esquerda(de trás para frente).
+const fruitsRight = fruits.reduceRight((acumulador, fruta) => {
+  return acumulador + " " + fruta; // Retorna o elemento anterior(acumulador) somado com a fruta(elemento atual).
+});
+console.log(fruitsRight); // Retorna no console a iteração de trás para frente.
+
+// Já o método reduce itera da esquerda para a direita(de frente para trás).
+const fruitsLeft = fruits.reduce((acumulador, fruta) => {
+  return acumulador + " " + fruta; // Retorna o elemento anterior(acumulador) somado com a fruta(elemento atual).
+});
+console.log(fruitsLeft); // Retorna no console a iteração de frente para trás.
+
+//* > Método some.
+
+// O método some passa por cada elemento da array e é usado para retornar um valor boolean se pelo menos uma iteração for true retornara true, se todas forem false ele retorna false.
+const hasGrape2 = fruits.some((fruta) => {
+  // Retorna true somente se a fruta for igual a uva, nesse caso não existe uvas na array então retorna false.
+  return fruta === "Uvas";
+});
+console.log(hasGrape2); // Retorna false no console pois não existe uma fruta chamada Uvas na array.
+
+const numbers1 = [0, 43, 22, 88, 101, 2]; // Criado uma array contendo tipo number.
+
+const hasBigger1 = numbers1.some((num) => num > 3); // Retorna true pois ele encontrou numbers na array que são maiores que 3, sendo assim teve valor true.
+console.log(hasBigger1); // Mostra true no console.
+
+//* > Método every.
+
+const moreFruits = ["Laranja", "Ameixa", "", "Maçã"];
+
+// O método every passa por cada elemento da array e é usado para retornar um valor boolean se pelo menos uma iteração for false irá retornar false, se todas forem true ele retorna true.
+const hasFruit = moreFruits.every((fruta) => {
+  // Retorna undefined pois pelo menos uma fruta está vazia '', o que é um valor false.
+  return fruta;
+});
+console.log(hasFruit); // Retorna false no console, pois não são todos elementos que são true.
+
+const numbers2 = [0, 43, 22, 88, 101, 2]; // Criado uma array contendo tipo number.
+
+const hasBigger2 = numbers2.every((num) => num > 10); // Retorna false pois ele encontrou numbers na array que não são maiores que 10, sendo assim teve valor false.
+console.log(hasBigger2); // Mostra true no console.
+
+//* > Método find e findIndex.
+
+// O find é um método que retorna o valor atual da primeira iteração que retornar um valor boolean true.
+const daysWeek = [1, 2, 3, 4, 5, 6, 7]; // Criado uma array chamada cities que contém elementos do tipo number.
+const searchDay = daysWeek.find((x) => x > 4); // Se x for maior que 4 ele irá retornar o número maior do que 4.
+console.log(searchDay); // Retorna 5 no console, pois 5 é o primeiro valor maior que 4(sendo assim true).
+
+const cities = ["São Paulo", "Rio de Janeiro", "Fortaleza", "Recife"]; // Criado uma array chamada cities que contém elementos do tipo string.
+
+// O método findIndex ao invés de retornar o valor, retorna o index/posição deste valor na array.
+const searchCity = cities.findIndex((cidade) => {
+  // Retorna o index do elemento da array encontrado.
+  return cidade === "Fortaleza";
+});
+console.log(searchCity); // Retorna 2 no console pois Fortaleza está no index 2.
+
+//* > Método filter.
+
+const names = ["Carolina", undefined, null, "", "Gabriel", 0, "Alessandra"]; // Criado uma array chamada names que contém elementos do tipo string, undefined, null e false.
+
+// O método filter retorna a array original mas com lista de valores que durante a sua iteração retornaram um valor true, sendo assim o que for false é ignorado.
+const cleanArray = names.filter((name) => {
+  // Retorna cada elemento da array names.
+  return name;
+});
+console.log(cleanArray); // Retorna somente os valores que são true no console.
+
+const randomNumbers = [6, 43, 22, 88, 101, 29]; // Criado uma array chamada names que contém elementos do tipo number.
+const searchNumber = randomNumbers.filter((x) => x > 45); // Retorna todo o número que o x for maior que 45.
+console.log(searchNumber); // Retorna a array filtrada somente com os elementos que são true conforma a condição passada no filter.
+
+//* > Método filter em objetos.
+
+// Criado uma array chamada moreClasses que contém 4 objetos.
+const moreClasses = [
+  {
+    name: "HTML 1",
+    min: 15,
+  },
+  {
+    name: "HTML 2",
+    min: 10,
+  },
+  {
+    name: "CSS 1",
+    min: 20,
+  },
+  {
+    name: "JS 1",
+    min: 25,
+  },
+];
+
+// O método filter retorna a array original mas com lista de valores que durante a sua iteração retornaram um valor true, sendo assim o que for false é ignorado.
+const greatestMinute = moreClasses.filter((aula) => {
+  // Retorna os elementos da array em que min teve o tempo de aula maior que 15.
+  return aula.min > 15;
+});
+console.log(greatestMinute);
