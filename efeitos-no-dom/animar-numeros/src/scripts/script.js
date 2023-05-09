a5fa4d09af2bb893db0c05e0af962fedf783edce
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-05-09 20:54:26 -0300

    update

---
+++ efeitos-no-dom/animar-numeros/src/scripts/script.js
@@ -0,0 +1,19 @@
+// O import é usado para chamar uma função por exemplo para importar dinamicamente um módulo que utiliza export.
+import initScrollToSection from "./modules/scroll-suave.js";
+import initAnimationScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tab-nav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+
+// Está executando as funções.
+initScrollToSection();
+initAnimationScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
