be8ef5081fe6ca60f46b7bb6ca086ae2ed9bc46e
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-05-24 22:46:42 -0300

    update

---
+++ javascript-assincrono/api-e-http/script.js
@@ -0,0 +1,101 @@
+//* Protocolo HTTP
+
+// Hypertext Transfer Protocol é o protocolo utilizando para enviarmos/recebermos arquivos e dados na Web, temos como exemplo uma api, que pode ser usada para enviar dados.
+fetch("https://pokeapi.co/api/v2/pokemon/")
+  // O parâmetro response é um objeto que possui um corpo com o conteúdo da resposta. Esse corpo pode ser transformado response métodos do protótipo do objeto Response. Estes retornam outras promises.
+  .then((response) => response.json())
+  // No parâmetro pokemon é colocado como valor o retorno do then anterior(no caso a JSON da API).
+  .then((pokemon) => {
+    console.log(pokemon); // Mostra no console a API em formato JSON.
+  });
+
+//* Url e Method.
+
+// Uma requisição HTTP é feita através de uma URL. O método padrão é o GET, mas existem outros como POST, UPDATE, DELETE, HEADER.
+
+const url1 = "https://jsonplaceholder.typicode.com/posts"; // A api armazenada na constante url está fazendo uma requisição HTTP.
+const options = {
+  method: "POST", // Tipo de método usado para criar/inserir uma informação nova.
+  body: '{"aula": "JavaScript"}', // Corpo do POST, que é os dados que estão sendo enviados, tem que seguir o formato JSON.
+  headers: {
+    "Content-Type": "application/json; charset=utf-8", // Está definindo o tipo de conteúdo que está sendo enviado como JSON no charset PT-BR(usado para caracteres, acentos do BR etc.)
+  },
+};
+
+// O fetch está recebendo dois parâmetros, a url e o método a ser usado. O method post não necessita de then, a não ser que queira ver a resposta enviada ao servidor.
+fetch(url1, options);
+
+//* Método GET.
+
+// O fetch está recebendo dois parâmetros, a url e o método a ser usado através de um objeto.
+fetch(url1, {
+  // O método GET irá puxar as informações da URL. Não é necessário informar que o método é GET, pois este é o padrão.
+  method: "GET", // O method é usado para definir o tipo de método a ser utilizado.
+})
+  // O parâmetro response é um objeto que possui um corpo com o conteúdo da resposta. Esse corpo pode ser transformado response métodos do protótipo do objeto Response. Estes retornam outras promises.
+  .then((response) => {
+    return response.json();
+  })
+  // No parâmetro JSON é colocado como valor o retorno do then anterior(no caso a JSON).
+  .then((json) => {
+    console.log(json); // Irá mostrar no console o que seria enviado no servidor dessa API conforme seu método.
+  });
+
+//* Método POST.
+
+// O fetch está recebendo dois parâmetros, a url e o método a ser usado através de um objeto. O method post não necessita de then, a não ser que queira ver a resposta que foi enviada ao servidor.
+fetch(url1, {
+  // O método POST irá criar uma nova postagem, utilizando o tipo de conteúdo especificado no headers e utilizando o conteúdo do body.
+  method: "POST", // O method é usado para definir o tipo de método a ser utilizado.
+  body: '{"title": "JavaScript"}', // Corpo do POST, que é os dados que estão sendo enviados, tem que seguir o formato JSON.
+  headers: {
+    "Content-Type": "application/json; charset=utf-8", // Está definindo o tipo de conteúdo que está sendo enviado como JSON no charset PT-BR(usado para caracteres, acentos do BR etc.)
+  },
+})
+  // O parâmetro response é um objeto que possui um corpo com o conteúdo da resposta. Esse corpo pode ser transformado response métodos do protótipo do objeto Response. Estes retornam outras promises.
+  .then((response) => {
+    return response.json();
+  })
+  // No parâmetro JSON é colocado como valor o retorno do then anterior(no caso a JSON).
+  .then((json) => {
+    console.log(json); // Irá mostrar no console o que seria enviado no servidor dessa API conforme seu método.
+  });
+
+//* > Método PUT.
+
+const url2 = "https://jsonplaceholder.typicode.com/posts/2";
+// O fetch está recebendo dois parâmetros, a url e o método a ser usado através de um objeto.
+fetch(url2, {
+  // O método PUT irá atualizar o conteúdo do URL com o que for informado no conteúdo do body.
+  method: "PUT", // O method é usado para definir o tipo de método a ser utilizado.
+  body: '{"title": "React"}', // Corpo do POST, que é os dados que estão sendo enviados, tem que seguir o formato JSON.
+  headers: {
+    "Content-Type": "application/json; charset=utf-8", // Está definindo o tipo de conteúdo que está sendo enviado como JSON no charset PT-BR(usado para caracteres, acentos do BR etc.)
+  },
+})
+  // O parâmetro response é um objeto que possui um corpo com o conteúdo da resposta. Esse corpo pode ser transformado response métodos do protótipo do objeto Response. Estes retornam outras promises.
+  .then((response) => {
+    return response.json();
+  })
+  // No parâmetro JSON é colocado como valor o retorno do then anterior(no caso a JSON).
+  .then((json) => {
+    console.log(json); // Irá mostrar no console o que seria enviado no servidor dessa API conforme seu método.
+  });
+
+//* > HEAD.
+
+// O fetch está recebendo dois parâmetros, a url e o método a ser usado através de um objeto.
+fetch(url2, {
+  // O HEAD puxa apenas os headers. É uma requisição mais leve pois não puxa o body.
+  method: "HEAD", // O method é usado para definir o tipo de método a ser utilizado.
+})
+  // O parâmetro response é um objeto que possui um corpo com o conteúdo da resposta. Esse corpo pode ser transformado response métodos do protótipo do objeto Response. Estes retornam outras promises.
+  .then((response) => {
+    response.headers.forEach(console.log); // Está acessando a propriedade headers do objeto Response, e passando por cada elemento do headers através do forEach, e por fim o forEach retorna essa iteração no console.
+
+    console.log(response.headers.get("Content-Type")); // Através do objeto Response acessa o headers e executa a função get que obtém e retorna algo, nesse caso o Content-Type(tipo de conteúdo) do objeto Response e mostra no console.
+  });
+
+//* > Headers.
+
+//* > Cors.
--- javascript-assincrono/json/script.js
+++ javascript-assincrono/json/script.js
@@ -19,24 +19,39 @@ fetch("./dados.json")
     });
   });
 
-//* > Método JSON.parse e JSON.stringiy.
+//* > Método JSON.parse.
 
-// O fetch faz a requisição no arquivo JSON criado e quando tiver o status de ok(carregado com sucesso) o then executa.
-fetch("./dados.json")
-  // O parâmetro response é um objeto que possui um corpo com o conteúdo da resposta. Esse corpo pode ser transformado response métodos do protótipo do objeto Response. Estes retornam outras promises.
-  .then((response) => {
-    return response.text(); // Retorna uma promise que tem como PromiseResult a transformação do que tem no arquivo dados.json para texto(string).
-  })
-  // O then está acessando o resultado do then anterior(no caso o json retornado) e aplicando ao parâmetro json criado.
-  .then((jsonText) => {
-    // O método JSON.parse irá transformar um texto JSON em um objeto JavaScript.
-    const jsonParse = JSON.parse(jsonText); // Armazena na constante jsonParse um JSON(em texto/string) que está sendo transformado pelo parse em um arquivo JSON.
-    console.log(jsonParse); // Mostra o arquivo em formato JSON no console.
-  });
+const textJSON = '{"id": 1, "title": "JavaScript", "time": "25min"}'; // Criado uma string chamada textJSON em formato JSON.
 
-// Já o método JSON.stringify irá transformar um objeto JavaScript em uma string no formato JSON.
-const configurations = {
+// O método JSON.parse irá transformar um texto JSON em um objeto JavaScript
+const textObject = JSON.parse(textJSON); // Armazena na constante textObject um JSON(em texto/string) que está sendo transformado pelo parse em um objeto.
+console.log(textObject); // Mostra o objeto no console.
+
+//* > Método JSON.stringify.
+
+// Criado um objeto chamado addressObject que contém 4 propriedades.
+const addressObject = {
+  cidade: "São Paulo",
+  rua: "Ali Perto",
+  pais: "Brasil",
+  numero: 190,
+};
+
+// O método JSON.stringify irá transformar um objeto JavaScript em uma string no formato JSON.
+const addressJSON = JSON.stringify(addressObject); // Transforma o objeto addressObject em uma string com o formato JSON e armazena na constante stringConfigs.
+console.log(addressJSON); // Mostra a string no console.
+
+//* > Exemplo real.
+
+// Criado um objeto chamado configs que contém 3 propriedades.
+const configs = {
   player: "Google",
   time: 25.5,
   course: "2.1 - Javascript",
 };
+
+localStorage.config = JSON.stringify(configs); // Cria e armazena no localStorage uma nova propriedade chamada config que recebe como seu valor a transformação de um objeto para uma string que segue o formato JSON.
+console.log(localStorage.config); // Puxa os valores armazenados no localStorage e mostra no console.
+
+const pullConfigsJSON = JSON.parse(localStorage.config); // Criado uma constante chamada ... que converte o texto de formato JSON presente no valor da propriedade config do localStorage em um objeto normal novamente.
+console.log(pullConfigsJSON); // Mostra no console o objeto.
