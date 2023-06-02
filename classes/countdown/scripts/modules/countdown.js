f93c4e50584c6448f61802a973852ec82da8fd71
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-06-01 23:01:13 -0300

    update

--- classes/countdown/scripts/modules/countdown.js
+++ classes/countdown/scripts/modules/countdown.js
@@ -1,3 +1,62 @@
 // Função responsável por fazer o countdown funcionar.
 // O export é usado para permitir que o código seja usado em outro arquivo JS. O default é geralmente usado para quando tem que exportar somente uma função ou classe do mesmo arquivo.
-export default class Countdown {}
+export default class Countdown {
+  // O método constructor é um método especial de uma classe. Nele você irá definir todas as propriedades do objeto que será criado. Os argumentos passados em new, vão direto para o constructor.
+  constructor(futureDate) {
+    this.futureDate = futureDate; // O this está referenciando o parâmetro futureDate criado no constructor.
+  }
+
+  // Criado um get que contém um método chamado _futureDate usado para retornar a data futura.
+  // O get é usado para obter informações e não pode ser alterado.
+  get _futureDate() {
+    return new Date(this.futureDate); // Retorna um novo objeto de data contendo como resultado a data definida no futureDate do constructor.
+  }
+
+  // Criado um get que contém um método chamado _actualDate usado para retornar a data atual.
+  // O get é usado para obter informações e não pode ser alterado.
+  get _actualDate() {
+    return new Date(); // Retorna um objeto contendo a data atual(conforme a máquina).
+  }
+
+  // Criado um get que contém um método chamado _timeStampDiff usado para retornar a diferença das datas em ms.
+  // O get é usado para obter informações e não pode ser alterado.
+  get _timeStampDiff() {
+    return this._futureDate.getTime() - this._actualDate.getTime(); // Retorna a diferença da data atual até a data futura em milissegundos.
+  }
+
+  get days() {
+    // Para fazer esse calculo pegamos o total de horas no dia(24h) multiplicado por 60m que resulta em 1 hora e multiplicado por 60s que é 1m e por fim multiplicado por 1000ms que é 1s.
+    return Math.floor(this._timeStampDiff / (24 * 60 * 60 * 1000)); // Pega os milissegundos e converte em dias e por fim retorna os números arredondados(para baixo).
+  }
+
+  get hours() {
+    // Para fazer esse calculo pegamos 60m que resulta em 1 hora e multiplicado por 60s que é 1m e por fim multiplicado por 1000ms que é 1s.
+    return Math.floor(this._timeStampDiff / (60 * 60 * 1000)); // Pega os milissegundos e converte em horas e por fim retorna os números arredondados(para baixo).
+  }
+
+  get minutes() {
+    // Para fazer esse calculo pegamos 60s que é 1m e por fim multiplicado por 1000ms que é 1s.
+    return Math.floor(this._timeStampDiff / (60 * 1000)); // Pega os milissegundos e converte em minutos e por fim retorna os números arredondados(para baixo).
+  }
+
+  get seconds() {
+    // Para fazer esse calculo pegamos 1000ms que é 1s.
+    return Math.floor(this._timeStampDiff / 1000); // Pega os milissegundos e converte em segundos e por fim retorna os números arredondados(para baixo).
+  }
+
+  get total() {
+    // Está acessando os métodos get(pelo this que referencia a class) e armazenando as informações do tempo até o natal convertidas e formatadas(usando o %) nas constantes.
+    const days = this.days;
+    const hours = this.hours % 24; // Está pegando as horas convertidas e dividindo esse valor por 24 e pegando o valor restante no divisor.
+    const minutes = this.minutes % 60; // Está pegando os minutos convertidas e dividindo esse valor por 60 e pegando o valor restante no divisor.
+    const seconds = this.seconds % 60; // Está pegando os minutos convertidas e dividindo esse valor por 60 e pegando o valor restante no divisor.
+
+    // Retorna um objeto contendo o nome do método e o valor dele em seguida.
+    return {
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+}
--- classes/countdown/scripts/script.js
+++ classes/countdown/scripts/script.js
@@ -1,5 +1,8 @@
 // O import é usado para chamar uma função por exemplo para importar dinamicamente um módulo que utiliza export.
-import initCountdown from "./modules/countdown";
+import Countdown from "./modules/countdown.js";
 
-// Está executando as funções.
-initCountdown();
+const timeToChristmas = new Countdown("22 June 2023 23:59:59 GMT-0300"); // Está preenchendo o parâmetro da class Countdown com uma string e armazenando como objeto na constante timeToChristmas.
+
+console.log("=-=-=-=-=-=-= TEMPO ATÉ MEU NIVER =-=-=-=-=-=-=");
+console.log(timeToChristmas.total);
+console.log("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
